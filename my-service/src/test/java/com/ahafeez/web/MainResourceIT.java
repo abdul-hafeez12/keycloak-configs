673d934c82b715aa2d025b2fd80d05639799dcae
Abdul Hafeez <ahafeez@teresol.com>
2025-05-02 16:55:37 +0500

    new

--- my-service/src/test/java/com/ahafeez/web/MainResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.ahafeez.web;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class MainResourceIT extends MainResourceTest {
-    // Execute the same tests but in packaged mode.
-}
